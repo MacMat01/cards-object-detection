57709fddcac4985b45c07871f44b47da99eecf57
macmat01 <matteo.machella01@gmail.com>
2024-04-17 18:13:28 +0200

    First implementation of card_detector.py

--- api/src/main/python/card_detection/card_detector.py
+++ api/src/main/python/card_detection/card_detector.py
@@ -4,38 +4,19 @@ import cv2 as cv
 
 
 class CardDetector:
-    def __init__(self, assets_folder=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')):
-        # Load the card images from the assets folder
-        self.card_images = {}
-        for card_name in ['Apple.png', 'Peach.png', 'Pear.png', 'Pineapple.png']:
-            image_path = os.path.join(assets_folder, card_name)
-            image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
-            if image is not None:
-                # Resize the image to ensure it is smaller than the frames
-                image = cv.resize(image, (100, 100))
-                self.card_images[card_name] = image
+    def __init__(self):
+        folder_path = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'resources'))
+        self.card_images = {
+            card_name: cv.resize(cv.imread(os.path.join(folder_path, card_name), cv.IMREAD_GRAYSCALE), (100, 100)) for
+            card_name in ['apple.png', 'apple_transparent.png', 'peach.png', 'peach_transparent.png', 'pear.png',
+                          'pear_transparent.png', 'pineapple.png', 'pineapple_transparent.png'] if
+            cv.imread(os.path.join(folder_path, card_name), cv.IMREAD_GRAYSCALE) is not None}
 
     def detect_face(self, frame):
-        # Convert the frame to grayscale
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-        # Initialize the best match values
-        best_match = None
-        best_match_value = 0
-
-        # Compare the frame to each card image using template matching
+        best_match, best_match_value = None, 0
         for card_name, card_image in self.card_images.items():
-            result = cv.matchTemplate(gray_frame, card_image, cv.TM_CCOEFF_NORMED)
-            _, max_val, _, _ = cv.minMaxLoc(result)
-
-            # If this match is better than the current best match, update the best match
+            _, max_val, _, _ = cv.minMaxLoc(cv.matchTemplate(gray_frame, card_image, cv.TM_CCOEFF_NORMED))
             if max_val > best_match_value:
-                best_match = card_name
-                best_match_value = max_val
-
-        # If no card image could be matched, return a default value
-        if best_match is None:
-            return 'No match found'
-
-        # Return the best match
-        return best_match
+                best_match, best_match_value = card_name, max_val
+        return best_match if best_match else 'No match found'
