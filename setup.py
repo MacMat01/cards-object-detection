815654df063071bd4bcc0ba24c184d14ae018546
macmat01 <matteo.machella01@gmail.com>
2024-05-31 13:20:14 +0200

    Added requests dependency

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='strategic-fruits-card-detection', version='0.1.0',
     author_email='matteo.machella01@gmail.com, cristian.marinozzi1@gmail.com',
     url='https://github.com/MacMat01/strategic-fruits-card-detection', packages=find_packages(),
     install_requires=['opendatasets', 'ultralytics', 'opencv-python', 'pandas', 'seaborn', 'matplotlib', 'ipykernel',
-        'torch', 'torchvision', 'torchaudio', 'imgaug', 'tqdm'],
+        'torch', 'torchvision', 'torchaudio', 'imgaug', 'tqdm', 'requests'],
     classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.12.3', ], )
