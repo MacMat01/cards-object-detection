31dd18fbce495a627b43cc214f1477f4f9baa294
macmat01 <matteo.machella01@gmail.com>
2024-06-04 13:00:14 +0200

    Updated README.md, environment.yml and setup.py to match new dependencies for QR Code detection

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 ﻿from setuptools import setup, find_packages
 
 setup(name='strategic-fruits-card-detection', version='0.1.1-alpha', license='MIT',
-    description='A project for detecting strategic fruits cards using YOLOv8', author='MacMat01, SenseiBonsai2k',
-    author_email='matteo.machella01@gmail.com, cristian.marinozzi1@gmail.com',
-    url='https://github.com/MacMat01/strategic-fruits-card-detection', packages=find_packages(),
-    install_requires=['opendatasets', 'ultralytics', 'opencv-python', 'pandas', 'seaborn', 'matplotlib', 'ipykernel',
-        'torch', 'torchvision', 'torchaudio', 'imgaug', 'tqdm', 'requests'],
-    classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.12.3', ], )
+      description='A project for detecting strategic fruits cards using YOLOv8', author='MacMat01, SenseiBonsai2k',
+      author_email='matteo.machella01@gmail.com, cristian.marinozzi1@gmail.com',
+      url='https://github.com/MacMat01/strategic-fruits-card-detection', packages=find_packages(),
+      install_requires=['opendatasets', 'ultralytics', 'opencv-python', 'pandas', 'seaborn', 'matplotlib', 'ipykernel',
+                        'torch', 'torchvision', 'torchaudio', 'imgaug', 'tqdm', 'requests', 'pyzbar'],
+      classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.12.3', ], )
