44eda6ba1feb114b5f6972eec67f052fae77f454
macmat01 <matteo.machella01@gmail.com>
2024-06-10 18:20:07 +0200

    Added delay of 2 seconds between detections and added check for consecutive same card detected for validation

--- src/main/python/app/real_time_app.py
+++ src/main/python/app/real_time_app.py
@@ -9,6 +9,7 @@ from ultralytics import YOLO
 
 class RealTimeApp:
     def __init__(self):
+        self.last_detected_card = None
         self.cards_set = ['1a', '2a', '3a', '4a', '5a', '1b', '2b', '3b', '4b', '5b', '1o', '2o', '3o', '4o', '5o',
                           '1p', '2p', '3p', '4p', '5p']
         self.write_api = self.initialize_influxdb()
@@ -63,11 +64,15 @@ class RealTimeApp:
 
         for card in self.cards_set:
             if card in detected_cards and card not in self.detected_cards_set:
-                self.detected_cards_set.add(card)
-                elapsed_time = time.time() - self.script_start_time
-                print(f"Card '{card}' has been detected after {elapsed_time} seconds.")
-                point = Point("card_detection").tag("card", card).field("elapsed_time", elapsed_time)
-                self.write_api.write(bucket="StrategicFruitsData", org="Cris&Matt", record=point.to_line_protocol())
+                if self.last_detected_card == card:
+                    self.detected_cards_set.add(card)
+                    elapsed_time = time.time() - self.script_start_time
+                    print(f"Card '{card}' has been detected after {elapsed_time} seconds.")
+                    point = Point("card_detection").tag("card", card).field("elapsed_time", elapsed_time)
+                    self.write_api.write(bucket="StrategicFruitsData", org="Cris&Matt", record=point.to_line_protocol())
+                    time.sleep(2)  # Wait for 2 seconds
+                else:
+                    self.last_detected_card = card
 
         cv2.imshow('Card Detection', detect_image)
         return True
